a41f9ea50462c96b72f982f7bcfb567a2f7fccb3
大前遼 <Ryo@Ryos-Macbook-Pro.local>
2018-12-20 02:07:16 +0900

    式改良

--- ホジキン-ハクスレイ.py
+++ ホジキン-ハクスレイ.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
+Hudgkin-Huxley方程式を元に実装
+http://www.hi.is.uec.ac.jp/rcb/paper/PDF/H18_wada.pdf
+数値は上記のサイトから引用
+'''
 class Neuron:
     C=1
     gNa=120
@@ -23,13 +28,18 @@ class Neuron:
         self.V=V0
 
     def calc(self,I):
+        alp_m=0.1*(40+self.V)/(1-np.exp(-(40+self.V)/10))
+        beta_m=4*np.exp(-(self.V+65)/18)
+        alp_h=0.07*np.exp(-(self.V+65)/20)
+        beta_h=1/(1+np.exp(-(self.V+35)/10))
+        
         m_next=(0.1*(40+self.V)/(1-np.exp(-(40+self.V)/10))*(1-self.m)-4*np.exp(-1*(self.V+65)/18)*self.m)*self.delta_t+self.m
-        h_next=(0.07*np.exp(-1*(self.V+65)/20)*(1-self.h)-1/(np.exp((35-self.V)/10)+1)*self.h)*self.delta_t+self.h
+        h_next=(0.07*np.exp(-1*(self.V+65)/20)*(1-self.h)-1/(np.exp((-35-self.V)/10)+1)*self.h)*self.delta_t+self.h
         n_next=(0.01*(55+self.V)/(1-np.exp(-1*(55+self.V)/10))*(1-self.n)-0.125*np.exp(-1*(self.V+65)/80)*self.n)*self.delta_t+self.n
         self.INa=self.gNa*(self.m**3)*self.h*(self.V-self.ENa)
         self.Ik=self.gk*(self.n**4)*(self.V-self.Ek)
         self.IL=self.gL*(self.V-self.EL)
-        V_next=(-1*(self.INa+self.Ik+self.IL)/self.C+I)*self.delta_t+self.V
+        V_next=((-1*(self.INa+self.Ik+self.IL)+I)/self.C)*self.delta_t+self.V
         self.m=m_next
         self.h=h_next
         self.n=n_next
@@ -49,9 +59,9 @@ class Neuron:
 #シミュレート設定
 #tは時間、Iは電流値[mA]
 
-t = 2000
-I = 200
-Nu=Neuron(-30)
+t = 4000
+I = 100
+Nu=Neuron(-50)
 
 x = np.linspace(0,t,t)
 y= []
